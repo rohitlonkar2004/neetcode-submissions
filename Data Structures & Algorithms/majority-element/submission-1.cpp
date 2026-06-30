e7834aa52c300c9d7ed199ae77a61ed188726640
Rohit Lonkar <144620309+rohitlonkar2004@users.noreply.github.com>
2026-06-30 15:42:05 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        unordered_map<int,int> mpp;
+
+        for( int i=0; i<nums.size(); i++ ) {
+            mpp[nums[i]]++;
+        }
+
+        for( int i=0; i<nums.size(); i++ ) {
+            if( mpp[nums[i]] > nums.size()/2 ) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
